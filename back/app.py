ee08439f824c45b22a6fcd52bc5568d97ae485fa
Anogio <antoine.ogier@gmail.com>
2022-11-04 00:04:59 +0100

    Add loader and backend cache

--- back/app.py
+++ back/app.py
@@ -4,6 +4,8 @@ from flask import abort
 import requests
 import random
 import datetime as dt
+from cachetools import cached, LRUCache
+
 app = Flask(__name__)
 
 QUESTIONS_PER_DAY = 5
@@ -14,24 +16,38 @@ with open("bot_token.txt") as f:
 
 @app.route("/puzzle")
 def puzzle():
-    response = requests.get("https://slack.com/api/users.list", headers={"Authorization": f"Bearer {BOT_TOKEN}"})
-    if response.status_code != 200:
-        abort(500, "Failed to get Slack users")
-
-    response_json = response.json()
-    users = [{"name": u["real_name"], "pictures": [u["profile"][f"image_{resolution}"]
-                                                   for resolution in [24, 32, 48, 72, 192]]}
-                for u in response_json["members"]
-             if not u["deleted"] and not u["is_bot"]  and not u["profile"].get("always_active")
-             and u["profile"].get("is_custom_image")]
-
     datetime_today = dt.datetime.combine(dt.date.today(), dt.time(0, 0, 0)).timestamp()
-    random.seed(datetime_today)
-    todays_puzzle = random.sample(population=users, k=QUESTIONS_PER_DAY)
-    result = flask.jsonify({
-        "puzzle": todays_puzzle,
-        "all_names": sorted([u["name"] for u in users])
-    })
-    result.headers.add('Access-Control-Allow-Origin', '*')
+    todays_puzzle, users = get_day_puzzle(timestamp=datetime_today)
+    result = flask.jsonify(
+        {"puzzle": todays_puzzle, "all_names": sorted([u["name"] for u in users])}
+    )
+    result.headers.add("Access-Control-Allow-Origin", "*")
     return result
 
+
+@cached(cache=LRUCache(maxsize=2))
+def get_day_puzzle(timestamp: float) -> tuple[list, list]:
+    response = requests.get(
+        "https://slack.com/api/users.list",
+        headers={"Authorization": f"Bearer {BOT_TOKEN}"},
+    )
+    if response.status_code != 200:
+        abort(500, "Failed to get Slack users")
+    response_json = response.json()
+    users = [
+        {
+            "name": u["real_name"],
+            "pictures": [
+                u["profile"][f"image_{resolution}"]
+                for resolution in [24, 32, 48, 72, 192]
+            ],
+        }
+        for u in response_json["members"]
+        if not u["deleted"]
+        and not u["is_bot"]
+        and not u["profile"].get("always_active")
+        and u["profile"].get("is_custom_image")
+    ]
+    random.seed(timestamp)
+    todays_puzzle = random.sample(population=users, k=QUESTIONS_PER_DAY)
+    return todays_puzzle, users
--- front/src/components/Main.vue
+++ front/src/components/Main.vue
@@ -1,92 +1,97 @@
 <template>
-  <div class="hello">
+  <div>
+    <loading :active="!loaded" :is-full-page="false" />
     <h1>Facedle</h1>
-    <h3>
-      Guess which of your coworkers are on the pictures ! They will get less
-      pixelated after each guess.
-    </h3>
-    <table id="puzzle" :style="{ width: '90%', margin: '0 auto' }">
-      <tr>
-        <td v-for="(p, index) in puzzle" :key="index">
-          <img
-            :src="
-              p.pictures[
-                finished
-                  ? p.pictures.length - 1
-                  : Math.min(nTries, maxTries - 1)
-              ]
-            "
-            :style="{ width: '200px', height: '200px' }"
-          />
-          <div v-for="(g, i) in p.pastGuesses" :key="i">
-            <n-tag
-              :style="{ margin: '5px' }"
-              :type="
-                g.match === 'exact'
-                  ? 'success'
-                  : g.match === 'partial'
-                  ? 'warning'
-                  : 'error'
+    <template v-if="loaded">
+      <h3>
+        Guess which of your coworkers are on the pictures ! They will get less
+        pixelated after each guess.
+      </h3>
+      <table id="puzzle" :style="{ width: '90%', margin: '0 auto' }">
+        <tr>
+          <td v-for="(p, index) in puzzle" :key="index">
+            <img
+              :src="
+                p.pictures[
+                  finished
+                    ? p.pictures.length - 1
+                    : Math.min(nTries, maxTries - 1)
+                ]
               "
-              :bordered="false"
-            >
-              {{ g.guess }}
-            </n-tag>
-            <br />
-          </div>
-          <div v-if="finished && !gameWon">
-            <n-tag :style="{ margin: '5px' }" type="info" :bordered="false">
-              {{ p.trueName }}
-            </n-tag>
-            <br />
-          </div>
-          <n-select
-            v-if="!finished"
-            :value="p.guess"
-            @update:value="(value) => (p.guess = value)"
-            :style="{ maxWidth: '200px', margin: 'auto' }"
-            placeholder="Please select a name"
-            :options="options"
-            filterable
-          />
-        </td>
-      </tr>
-    </table>
-    <br />
-    <div v-if="finished">
-      <div v-if="gameWon">Congratulations ! You won. See you tomorrow :)</div>
-      <div v-else>You lost ! Try again tomorrow :)</div>
+              :style="{ width: '200px', height: '200px' }"
+            />
+            <div v-for="(g, i) in p.pastGuesses" :key="i">
+              <n-tag
+                :style="{ margin: '5px' }"
+                :type="
+                  g.match === 'exact'
+                    ? 'success'
+                    : g.match === 'partial'
+                    ? 'warning'
+                    : 'error'
+                "
+                :bordered="false"
+              >
+                {{ g.guess }}
+              </n-tag>
+              <br />
+            </div>
+            <div v-if="finished && !gameWon">
+              <n-tag :style="{ margin: '5px' }" type="info" :bordered="false">
+                {{ p.trueName }}
+              </n-tag>
+              <br />
+            </div>
+            <n-select
+              v-if="!finished"
+              :value="p.guess"
+              @update:value="(value) => (p.guess = value)"
+              :style="{ maxWidth: '200px', margin: 'auto' }"
+              placeholder="Please select a name"
+              :options="options"
+              filterable
+            />
+          </td>
+        </tr>
+      </table>
       <br />
-      <n-button type="info" @click="copyShare">
-        <template #icon>
-          <n-icon> <clipboard /> </n-icon>
-        </template>
-        {{ shared ? "Copied !" : "Share" }}</n-button
-      >
-    </div>
-    <div v-else>
-      <n-statistic
-        :value="nTries"
-        :style="{
-          display: 'inline-block',
-          marginRight: '10px',
-        }"
-      >
-        <template #suffix> / {{ maxTries }} guesses </template>
-      </n-statistic>
-      <n-button type="info" round :disabled="!canSubmit" @click="handleSubmit"
-        >Submit</n-button
-      >
-    </div>
+      <div v-if="finished">
+        <div v-if="gameWon">Congratulations ! You won. See you tomorrow :)</div>
+        <div v-else>You lost ! Try again tomorrow :)</div>
+        <br />
+        <n-button type="info" @click="copyShare">
+          <template #icon>
+            <n-icon> <clipboard /> </n-icon>
+          </template>
+          {{ shared ? "Copied !" : "Share" }}</n-button
+        >
+      </div>
+      <div v-else>
+        <n-statistic
+          :value="nTries"
+          :style="{
+            display: 'inline-block',
+            marginRight: '10px',
+          }"
+        >
+          <template #suffix> / {{ maxTries }} guesses </template>
+        </n-statistic>
+        <n-button type="info" round :disabled="!canSubmit" @click="handleSubmit"
+          >Submit</n-button
+        >
+      </div>
+    </template>
   </div>
 </template>
 
 <script>
 import { Clipboard } from "@vicons/ionicons5";
+import Loading from "vue-loading-overlay";
+import "vue-loading-overlay/dist/css/index.css";
 
 export default {
   name: "HelloWorld",
-  components: { Clipboard },
+  components: { Clipboard, Loading },
   data: () => {
     return {
       puzzle: [],
@@ -96,6 +101,7 @@ export default {
       nTries: 0,
       summary: "",
       shared: false,
+      loaded: false,
     };
   },
   async created() {
@@ -108,6 +114,7 @@ export default {
       pastGuesses: [],
     }));
     this.options = payload.all_names.map((el) => ({ label: el, value: el }));
+    this.loaded = true;
   },
   computed: {
     canSubmit() {
