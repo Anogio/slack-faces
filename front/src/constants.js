d5507d34124379c5d94f06b6144095af4920c238
Anogio <antoine.ogier@gmail.com>
2022-11-15 14:31:27 +0100

    Fix env variables

--- front/src/constants.js
+++ front/src/constants.js
@@ -1,2 +1,2 @@
-export const SERVER_URL = process.env.SERVER_URL || "http://127.0.0.1:5000";
-export const APP_URL = process.env.APP_URL || "https://localhost:8080";
+export const SERVER_URL = process.env.VUE_APP_SERVER_URL;
+export const APP_URL = process.env.VUE_APP_APP_URL;
